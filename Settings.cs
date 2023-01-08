3199d24ab2d0c4347690206fbea45727ba3d4a2f
ajkarell <111962031+ajkarell@users.noreply.github.com>
2023-01-08 22:08:27 +0200

    enable frustum culling by default

--- Settings.cs
+++ Settings.cs
@@ -2,7 +2,7 @@ namespace MonoCraft;
 
 internal static class Settings
 {
-    public static readonly bool UseFrustumCulling = false;
+    public static readonly bool UseFrustumCulling = true;
     public static readonly int RenderDistanceChunks = 5;
     public static readonly int WorldGenThresholdHorizontal = 2;
     public static readonly int WorldGenThresholdVertical = 1;
